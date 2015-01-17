aaa746fc111db684e13efca8ebe1cacdbf61078d
Igor Babuschkin <igor@babuschk.in>
2015-01-17 15:08:27 +0100

    Implement sqrt(N) error bars

--- missing_hep/__init__.py
+++ missing_hep/__init__.py
@@ -9,13 +9,16 @@ def calc_nbins(x, maximum=150):
     n =  (max(x) - min(x)) / (2 * len(x)**(-1/3) * (np.percentile(x, 75) - np.percentile(x, 25)))
     return min(n, maximum)
 
-def bin_errors(N, errors, confidence=0.6827):
+def poisson_limits(N, kind, confidence=0.6827):
     alpha = 1 - confidence
     upper = np.zeros(len(N))
     lower = np.zeros(len(N))
-    if errors == 'gamma':
+    if kind == 'gamma':
         lower = stats.gamma.ppf(alpha / 2, N)
         upper = stats.gamma.ppf(1 - alpha / 2, N + 1)
+    if kind == 'sqrt':
+        lower = sqrt(N)
+        upper = lower
     else:
         raise ValueError('Unknown distribution: {}'.format(errors))
     # clip lower bars
@@ -33,12 +36,13 @@ def histpoints(x, nbins=None, xerr=None, yerr='gamma', *kargs, **kwargs):
     center = (bins[:-1] + bins[1:]) / 2
     area = sum(h * width)
 
-    lower, upper = bin_errors(h, yerr)
+    if isintance(yerr, basestring):
+        yerr = poisson_limits(h, yerr)
 
     if xerr == 'binwidth':
         xerr = width / 2
 
-    plt.errorbar(center, h, xerr=xerr, yerr=(lower, upper), fmt='o', color='black', markersize=5)
+    plt.errorbar(center, h, xerr=xerr, yerr=yerr, fmt='o', color='black', markersize=5)
 
     return center, h, area
 
