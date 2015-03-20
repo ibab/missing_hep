c4d9c0f623e8417fb26adb91a8e05d718948aa56
Igor Babuschkin <igor@babuschk.in>
2015-03-20 16:16:12 +0100

    Fix lots of bugs

--- missing_hep/__init__.py
+++ missing_hep/__init__.py
@@ -1,9 +1,11 @@
 
+from __future__ import division
 __all__ = ['calc_nbins', 'histpoints']
 
 import numpy as np
 import scipy.stats as stats
 import scipy as sp
+import logging
 
 def calc_nbins(x, maximum=150):
     n =  (max(x) - min(x)) / (2 * len(x)**(-1/3) * (np.percentile(x, 75) - np.percentile(x, 25)))
@@ -16,33 +18,46 @@ def poisson_limits(N, kind, confidence=0.6827):
     if kind == 'gamma':
         lower = stats.gamma.ppf(alpha / 2, N)
         upper = stats.gamma.ppf(1 - alpha / 2, N + 1)
-    if kind == 'sqrt':
+    elif kind == 'sqrt':
         lower = sqrt(N)
         upper = lower
     else:
-        raise ValueError('Unknown distribution: {}'.format(errors))
+        raise ValueError('Unknown distribution: {}'.format(kind))
     # clip lower bars
     lower[N==0] = 0
     return N - lower, upper - N
 
-def histpoints(x, nbins=None, xerr=None, yerr='gamma', *kargs, **kwargs):
+def histpoints(x, bins=None, xerr=None, yerr='gamma', normed=False, **kwargs):
     import matplotlib.pyplot as plt
 
-    if not nbins:
-        nbins = calc_nbins(x)
+    if bins is None:
+        bins = calc_nbins(x)
 
-    h, bins = np.histogram(x, bins=calc_nbins(x), *kargs, **kwargs)
+    h, bins = np.histogram(x, bins=bins)
     width = bins[1] - bins[0]
     center = (bins[:-1] + bins[1:]) / 2
     area = sum(h * width)
 
-    if isintance(yerr, basestring):
+    if isinstance(yerr, str):
         yerr = poisson_limits(h, yerr)
 
     if xerr == 'binwidth':
         xerr = width / 2
 
-    plt.errorbar(center, h, xerr=xerr, yerr=yerr, fmt='o', color='black', markersize=5)
+    if normed:
+        h = h / area
+        yerr = yerr / area
+
+    if not 'color' in kwargs:
+        kwargs['color'] = 'black'
+
+    if not 'fmt' in kwargs:
+        kwargs['fmt'] = 'o'
+
+    if not 'markersize' in kwargs:
+        kwargs['markersize'] = 5
+
+    plt.errorbar(center, h, xerr=xerr, yerr=yerr, **kwargs)
 
     return center, h, area
 
